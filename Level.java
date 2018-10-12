0757e48f1dae0364b2ba6df773b984d5bb1f096d
Linki <Linki@141.71.164.50>
2018-10-12 12:06:36 +0200

    neuer test commit

--- Level.java
+++ Level.java
@@ -5,6 +5,7 @@ public abstract class Level extends World {
     public Level() {    
         super(800, 600, 1);
         addObject(new Barrel(), 100, 100);
+        // neuer test
         // das hier ist ein test
     }
 }
