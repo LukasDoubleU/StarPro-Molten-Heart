a63360202b922c6425231e99d055dcd3a0cebca3
heroberlin <heroberlin@github.com>
2018-10-12 11:51:50 +0200

    Update Level.java
    awd

--- Level.java
+++ Level.java
@@ -3,6 +3,7 @@ import greenfoot.*;
 public abstract class Level extends World {
 
     public Level() {    
-        super(800, 600, 1); 
+        super(800, 600, 1);
+        addObject(new Barrel(), 100, 100);
     }
 }
