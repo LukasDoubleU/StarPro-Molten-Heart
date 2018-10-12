76a7926961bd2214d29f36001d60533299cc4aaf
fiae15hbrandt <jannicklasb@gmail.com>
2018-10-12 11:57:23 +0200

    Merge-Fehler behoben

--- Level.java
+++ Level.java
@@ -4,10 +4,7 @@ public abstract class Level extends World {
 
     public Level() {    
         super(800, 600, 1);
-<<<<<<< HEAD
         addObject(new Barrel(), 100, 100);
-=======
         // das hier ist ein test
->>>>>>> 22480ceb62d980ecb9444af4a646966821b080a7
     }
 }
