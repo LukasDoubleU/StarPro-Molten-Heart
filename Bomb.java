e7d165d361e496e6d4549c643ad063cfd1d30ccc
Komtoo <proroxkomto@live.de>
2018-10-12 13:46:25 +0200

    Update Bomb.java
    Bombe Bewegung hinzugefügt, Spieler wird erkannt

--- Bomb.java
+++ Bomb.java
@@ -1,11 +1,32 @@
 import greenfoot.*;
+import java.util.*;
 
 public class Bomb extends Enemy {
     
-    //das ist ein Kommentar
-    
+    int counter = 0;
+    boolean foundplayer = false;
 
     public void act() {
+        
+        List actorinrange = new ArrayList();
+        actorinrange = this.getObjectsInRange(30, Player.class);
+        
+        for(Object a : actorinrange)  {
+            if(a instanceof Player) {
+                
+            }
+            
+        }
+        
+        
+        
+        if(counter==40){
+            turn(90);
+            counter = 0;
+        }
+        
+        move(3);
+        counter = counter + 1;
 
     }    
 }
