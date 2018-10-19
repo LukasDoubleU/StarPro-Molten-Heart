d37ab86e67c50ef7fc07e463c5551ce766b30f81
Mantanismus <44084142+Mantanismus@users.noreply.github.com>
2018-10-19 13:38:11 +0200

    level wall add

---
+++ Wall.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+
+public class Wall extends Obstacle {
+
+    
+    
+    
+    public Wall(int i) {
+        setRotation(i);
+        setImage("brick_wall.png");
+    }
+    
+    
+    public void act() {
+        
+        
+    }
+    
+    
+}
