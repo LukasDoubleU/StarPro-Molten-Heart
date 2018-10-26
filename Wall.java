6a1e23584e7852cde5bba167367ad8878a460882
Mantanismus <44084142+Mantanismus@users.noreply.github.com>
2018-10-26 10:15:54 +0200

    World assets

--- Wall.java
+++ Wall.java
@@ -5,9 +5,10 @@ public class Wall extends Obstacle {
     
     
     
-    public Wall(int i) {
-        setRotation(i);
-        setImage("brick_wall.png");
+    public Wall() {
+        
+        setImage("transparent.png");
+        
     }
     
     
