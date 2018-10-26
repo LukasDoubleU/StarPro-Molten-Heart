89ea0cba7c02c3c7ee6d2c7cd18456750b337867
fiae15hbrandt <jannicklasb@gmail.com>
2018-10-26 10:00:28 +0200

    Encoding Fehler behoben

--- Weapon.java
+++ Weapon.java
@@ -12,7 +12,7 @@ public abstract class Weapon extends Item {
     }
 
     /**
-     * Führt einen Angriff aus, in die Blickrichtung des Spielers.
+     * FÃ¼hrt einen Angriff aus, in die Blickrichtung des Spielers.
      */
     public abstract void attack();
 
