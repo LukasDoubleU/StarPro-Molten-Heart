679e2d4347497e25bc3c09cfc60e7ed81afefe9f
Mantanismus <44084142+Mantanismus@users.noreply.github.com>
2018-11-23 10:24:25 +0100

    level fix

--- Level.java
+++ Level.java
@@ -153,10 +153,7 @@ public abstract class Level extends World {
             object = new Potion.Speed();
         } else if (obj == 95) {
             object = new MoltenHeart();
-        } else if (obj == 94) {
-            object = new Footer();
         }
-
         if (object != null) {
             addObject(object, 10 + x, 10 + y);
         }
