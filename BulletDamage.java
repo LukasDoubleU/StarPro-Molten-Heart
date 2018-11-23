c8ffa00b3725b8d90c390a16cc50257e7648024d
heroberlin <heroberlin@github.com>
2018-11-23 10:07:09 +0100

    BulletDamage() geändert, LavaBoss() geändert

--- BulletDamage.java
+++ BulletDamage.java
@@ -18,9 +18,12 @@ public class BulletDamage extends Projectiles
     int mov_speed;
     int damage;
     int turnDegree;
+    int knockBackX;
+    int knockBackY;
     Player target = null;
     boolean turnNotDone;
     boolean isAlive;
+    boolean knockBack = false;
 
     public BulletDamage(int newMov_Speed, int newDamage, Player newTarget, String imgPath) {
         mov_speed = newMov_Speed;
@@ -34,6 +37,8 @@ public class BulletDamage extends Projectiles
         turnNotDone = true;
     }
     
+    
+    
     public BulletDamage(int newMov_Speed, int newDamage, int newTurnDegree, Player newTarget, String imgPath) {
         mov_speed = newMov_Speed;
         damage = newDamage;
@@ -46,6 +51,22 @@ public class BulletDamage extends Projectiles
         turnNotDone = true;
         turnDegree = newTurnDegree;
     }
+    
+    public BulletDamage(int newMov_Speed, int newDamage, int newTurnDegree, Player newTarget, String imgPath, int knockBackXX, int knockBackYY) {
+        mov_speed = newMov_Speed;
+        damage = newDamage;
+        target = newTarget;
+        setImage(imgPath);
+        isAlive = true;
+        turnNotDone = true;
+        turnDegree = newTurnDegree;
+        this.knockBackX = knockBackXX;
+        this.knockBackY = knockBackYY;
+        this.knockBack = true;
+        getImage().scale(90,90);
+    }
+    
+    
     
     public void act() 
     {
@@ -74,14 +95,25 @@ public class BulletDamage extends Projectiles
     
     public void checkCollision() {
         List intersectingObjects = new ArrayList();
-        intersectingObjects = this.getObjectsInRange(20, null); 
+        intersectingObjects = this.getObjectsInRange(20, null);
+        if(this.knockBack){
+            intersectingObjects = this.getObjectsInRange((this.getImage().getHeight()/2)+10, null);
+        }
+
         
         for(Object a : intersectingObjects)  {          
             if(a instanceof Player) {
-                Player.get().damage(damage);
-                this.getWorld().removeObject(this);
-                isAlive = false;
-                return;
+                if(this.knockBack){
+                    Player.get().setLocation(this.knockBackX, this.knockBackY);
+                    this.getWorld().removeObject(this);
+                    isAlive = false;
+                    return;
+                }else{
+                    Player.get().damage(damage);
+                    this.getWorld().removeObject(this);
+                    isAlive = false;
+                    return;
+                }
             }
             if((a instanceof Obstacle) && !(a instanceof Enemy)) {
                 this.getWorld().removeObject(this);
