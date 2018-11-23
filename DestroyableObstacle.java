a4d73f199179f73c082bfef55043adc24384f038
Komtoo <proroxkomto@live.de>
2018-11-23 12:16:23 +0100

    Fix

--- DestroyableObstacle.java
+++ DestroyableObstacle.java
@@ -5,17 +5,22 @@
  * @version (a version number or a date)
  */
 public class DestroyableObstacle extends Enemy {
-
+    
     public DestroyableObstacle(double x) {
 
         if (x == 1) {
-            super(0, 1, "img_utilities/rock.png");
+            this(0, 1, "img_utilities/rock.png");
         } else if (x == 2) {
-            super(0, 1, "img_utilities/barrel.png");
+            this(0, 1, "img_utilities/barrel.png");
         } else if (x == 3) {
-            super(0, 1, "img_utilities/slime.png");
+            this(0, 1, "img_utilities/slime.png");
         }else {
-            super(0, 1, "img_utilities/slime.png");
+            this(0, 1, "img_utilities/slime.png");
         }
     }
+
+    public DestroyableObstacle(int newMov_speed, int newLifeCount, String imgPath) {
+        super(newMov_speed, newLifeCount, imgPath);
+    }
+    
 }
