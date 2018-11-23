152c5da6b9eac9e32874d29798050cc17d37f26e
heroberlin <heroberlin@github.com>
2018-11-23 08:49:04 +0100

    neue bilder, funktion überarbeitet

---
+++ Hand.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Hand here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hand extends Boss
+{
+  
+    public Hand(){
+        setImage("boss/boss1hand.png");
+        this.lifeCount = 5;
+    }
+    
+    public void act() 
+    {
+        
+    }    
+}
