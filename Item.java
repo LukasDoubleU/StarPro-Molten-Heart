988a8e22ac4894700685e1ca21e34dfb94dc664b
Lukas Wulff <lukaswulff@gmx.net>
2018-12-21 12:07:22 +0100

    HighScore Sound

--- Item.java
+++ Item.java
@@ -2,27 +2,10 @@
 import greenfoot.Actor;
 
 /**
- * Items liegen in der Welt verteilt oder werden von Gegnern fallen gelassen.
+ * Items liegen in der Welt verteilt.
  * Items können z.B. Lebenstränke oder Waffen sein.
  */
 public abstract class Item extends Actor {
-
-    public enum Rarity {
-
-        Common(0.8f, new Sword.Big(), new Potion.Health()),
-
-        Rare(0.5f, new Armor.Bright(), new Potion.Speed(), new Potion.Damage(), new Bow()),
-
-        Legendary(0.2f, new Sword.Master(), new Armor.Dark(), new Potion.Immortality());
-
-        float dropChance;
-        Item[] items;
-
-        Rarity(float dropChance, Item... items) {
-            this.dropChance = dropChance;
-            this.items = items;
-        }
-    }
 
     /**
      * @param image Name des Bildes, das das Item repräsentiert (auf dem Boden und
